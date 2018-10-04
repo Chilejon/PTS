c9cd879c3d3b265284041e0560f6fa8602c96d18
Jon Chiles <jonchiles@yahoo.com>
2018-10-04 09:45:41 +0100

    Added start of table

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,8 @@ class Home extends Component {
   state = {
     weekInfo: 
     [
-      { weekNo: 1, weekDate: '01/01/2019' }
+      { weekNo: 1, weekDate: '01/01/2019', name: 'Jon Chiles', h1: 1, a1: 0, h2: 2, a2: 1, h3: 3, a3: 0, h4: 2, a4: 1, h5: 2, a5: 3 },
+      { weekNo: 1, weekDate: '01/01/2019', name: 'Tony Blaikie', h1: 2, a1: 1, h2: 3, a2: 2, h3: 3, a3: 1, h4: 4, a4: 1, h5: 2, a5: 1 }
     ]
   };
 
@@ -14,9 +15,19 @@ class Home extends Component {
     return (
       <section>
           <Predictions />
+          <section className="ptsFullTable">
+          <table>
           {this.state.weekInfo.map(allPredictions => (
-          <AllPredictions weekNo={allPredictions.weekNo} weekDate={allPredictions.weekDate} />
+          <AllPredictions weekNo={allPredictions.weekNo} weekDate={allPredictions.weekDate} name={allPredictions.name}
+          h1={allPredictions.h1} a1={allPredictions.a1}
+          h2={allPredictions.h2} a2={allPredictions.a2}
+          h3={allPredictions.h3} a3={allPredictions.a3}
+          h4={allPredictions.h4} a4={allPredictions.a4}
+          h5={allPredictions.h5} a5={allPredictions.a5}
+          />
         ))}
+        </table>
+        </section>
       </section>
     );
   }
--- src/components/AllPredictions.js
+++ src/components/AllPredictions.js
@@ -3,7 +3,18 @@ import React, { Component } from "react";
 class AllPredictions extends Component {
   state = {
     weekNo : this.props.weekNo,
-    weekDate : this.props.weekDate
+    weekDate : this.props.weekDate,
+    name: this.props.name,
+    h1: this.props.h1,
+    a1: this.props.a1,
+    h2: this.props.h2,
+    a2: this.props.a2,
+    h3: this.props.h3,
+    a3: this.props.a3,
+    h4: this.props.h4,
+    a4: this.props.a4,
+    h5: this.props.h5,
+    a5: this.props.a5
   };
 
   
@@ -11,10 +22,12 @@ class AllPredictions extends Component {
   render() {
     console.log('props', this.props)
     return (
-      <section className="ptsFullTable">
-
-        <label>Week No: {this.props.weekNo} Date: {this.props.weekDate}</label>
-      </section>
+        <tr><td>{this.props.name}</td>
+        <td>{this.props.h1}</td><td>{this.props.a1}</td>
+        <td>{this.props.h2}</td><td>{this.props.a2}</td>
+        <td>{this.props.h3}</td><td>{this.props.a3}</td>
+        <td>{this.props.h4}</td><td>{this.props.a4}</td>
+        <td>{this.props.h5}</td><td>{this.props.a5}</td></tr>
     );
   }
 }
