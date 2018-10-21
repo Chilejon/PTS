92e3c2535096f2410310830389208a5d882fcf3f
Chilejon <jonchiles@yahoo.com>
2018-10-21 11:57:24 +0100

    indipendant sliders@

--- src/Home.js
+++ src/Home.js
@@ -3,81 +3,123 @@ import Predictions from "./Predictions";
 import AllPredictions from "./components/AllPredictions";
 import Slider from "./components/Slider";
 
-const API = 'https://hn.algolia.com/api/v1/search?query='
-const API2 = 'http://interactive.stockport.gov.uk/siarestapi/v1/Getareas'
-const API3 = 'http://interactive.stockport.gov.uk/siarestapi/v1/GetPhotosByID?id=3'
-const API4 = 'http://interactive.stockport.gov.uk/siarestapi/v1/GetPhotosByTitle/?term=pineapple'
+const API = "https://hn.algolia.com/api/v1/search?query=";
+const API2 = "http://interactive.stockport.gov.uk/siarestapi/v1/Getareas";
+const API3 =
+  "http://interactive.stockport.gov.uk/siarestapi/v1/GetPhotosByID?id=3";
+const API4 =
+  "http://interactive.stockport.gov.uk/siarestapi/v1/GetPhotosByTitle/?term=pineapple";
 
-const DEFAULT_QUERY = 'redux';
-const DEFAULT_QUERY2 = '';
-
+const DEFAULT_QUERY = "redux";
+const DEFAULT_QUERY2 = "";
 
 class Home extends Component {
   state = {
-    weekInfo: 
-    [
-      { weekNo: 1, weekDate: '01/01/2019', name: 'Jon Chiles', h1: 1, a1: 0, h2: 2, a2: 1, h3: 3, a3: 0, h4: 2, a4: 1, h5: 2, a5: 3 },
-      { weekNo: 1, weekDate: '01/01/2019', name: 'Tony Blaikie', h1: 2, a1: 1, h2: 3, a2: 2, h3: 3, a3: 1, h4: 4, a4: 1, h5: 2, a5: 1 }
+    weekInfo: [
+      {
+        weekNo: 1,
+        weekDate: "01/01/2019",
+        name: "Jon Chiles",
+        h1: 1,
+        a1: 0,
+        h2: 2,
+        a2: 1,
+        h3: 3,
+        a3: 0,
+        h4: 2,
+        a4: 1,
+        h5: 2,
+        a5: 3
+      },
+      {
+        weekNo: 1,
+        weekDate: "01/01/2019",
+        name: "Tony Blaikie",
+        h1: 2,
+        a1: 1,
+        h2: 3,
+        a2: 2,
+        h3: 3,
+        a3: 1,
+        h4: 4,
+        a4: 1,
+        h5: 2,
+        a5: 1
+      }
     ],
     data: [],
     data2: [],
     photo: {}
   };
 
-
   componentDidMount() {
     fetch(API2)
       .then(response => response.json())
-      .then(json => {console.log(json);
+      .then(json => {
+        console.log(json);
         this.setState({
           data: json
         });
       });
 
-      fetch(API4)
+    fetch(API4)
       .then(response => response.json())
-      .then(json => {console.log(json);
+      .then(json => {
+        console.log(json);
         this.setState({
           data2: json
         });
       });
   }
 
-
   render() {
     var canvases = this.state.data.map(function(data) {
       return (
-        <section id={data.ID}>{data.ID} : {data.Area1}</section>
+        <section id={data.ID}>
+          {data.ID} : {data.Area1}
+        </section>
       );
     });
 
     var canvases2 = this.state.data2.map(function(data2) {
       return (
-        <li>{data2.title} :  {data2.description}</li>
+        <li>
+          {data2.title} : {data2.description}
+        </li>
       );
     });
 
     return (
       <section>
-          <Predictions />
-          <section className="ptsFullTable">
+        <Predictions />
+        <section className="ptsFullTable">
           <table>
-          {this.state.weekInfo.map(allPredictions => (
-          <AllPredictions weekNo={allPredictions.weekNo} weekDate={allPredictions.weekDate} name={allPredictions.name}
-          h1={allPredictions.h1} a1={allPredictions.a1}
-          h2={allPredictions.h2} a2={allPredictions.a2}
-          h3={allPredictions.h3} a3={allPredictions.a3}
-          h4={allPredictions.h4} a4={allPredictions.a4}
-          h5={allPredictions.h5} a5={allPredictions.a5}
-          />
-        ))}
-        </table>
+            {this.state.weekInfo.map(allPredictions => (
+              <AllPredictions
+                weekNo={allPredictions.weekNo}
+                weekDate={allPredictions.weekDate}
+                name={allPredictions.name}
+                h1={allPredictions.h1}
+                a1={allPredictions.a1}
+                h2={allPredictions.h2}
+                a2={allPredictions.a2}
+                h3={allPredictions.h3}
+                a3={allPredictions.a3}
+                h4={allPredictions.h4}
+                a4={allPredictions.a4}
+                h5={allPredictions.h5}
+                a5={allPredictions.a5}
+              />
+            ))}
+          </table>
         </section>
-        <span>
-        <Slider id="A" team="Team A" score='-4' />
-        </span>
-       
+        <section className="slider">
+          <Slider id="A" team="Team A" score="-4" />
         </section>
+        <section className="slider">
+          <Slider id="B" team="Team B" score="-2" />
+        </section>
+      </section>
     );
   }
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,112 +5,141 @@ class Slider extends Component {
     super(props, context);
 
     this.state = {
-    team: this.props.team,
-    score: this.props.score
-  };
+      team: this.props.team,
+      score: this.props.score,
+      id: this.props.id
+    };
 
-  this.moveTokenBack = this.moveTokenBack.bind(this)
-  this.moveTokenForward = this.moveTokenForward.bind(this)
+    this.moveTokenBack = this.moveTokenBack.bind(this);
+    this.moveTokenForward = this.moveTokenForward.bind(this);
+  }
 
+  componentDidMount() {
+    var score = parseInt(this.state.score);
+    var tokenLocation = this.state.id + score;
+    if (score > 0) {
+      document.getElementById(tokenLocation).className = "token";
+    }
+    if (score === 0) {
+      document.getElementById(tokenLocation).className = "token";
+    }
+    if (score < 0) {
+      document.getElementById(tokenLocation).className = "token";
+    }
+  }
+
+  moveTokenBack(event) {
+    score = this.state.score;
+    var score = parseInt(this.state.score);
+    var tokenLocation = this.state.id + score;
+
+    if (score < 0) {
+      document.getElementById(tokenLocation).className = "minus";
+    }
+    if (score === 0) {
+      document.getElementById(tokenLocation).className = "even";
+    }
+    if (score > 0) {
+      document.getElementById(tokenLocation).className = "plus";
     }
 
-    componentDidMount() {
-      var score = parseInt(this.state.score)
-      if(score > 0){
-      var tokenLocation = score
-      document.getElementById(tokenLocation).className = "token"
-      }
-      if(score === 0){
-        
-        var tokenLocation = score
-        document.getElementById(tokenLocation).className = "token"
-        }
-        if(score < 0){
-          var tokenLocation = score
-          document.getElementById(tokenLocation).className = "token"
-          }
-            
+    score = score - 1;
+    this.setState({ score: score });
+    tokenLocation = this.state.id + score;
+    document.getElementById(tokenLocation).className = "token";
+  }
+
+  moveTokenForward(event) {
+    score = this.state.score;
+    var score = parseInt(this.state.score);
+    var tokenLocation = this.state.id + score;
+
+    if (score < 0) {
+      document.getElementById(tokenLocation).className = "minus";
+    }
+    if (score === 0) {
+      document.getElementById(tokenLocation).className = "even";
+    }
+    if (score > 0) {
+      document.getElementById(tokenLocation).className = "plus";
     }
 
-    moveTokenBack(event) {
-      score = this.state.score
-      var score = parseInt(this.state.score)
-      
-      if(score < 0)
-      {
-        document.getElementById(score).className = "minus"
-      }
-      if(score === 0)
-      {
-        document.getElementById(score).className = "even"
-      }
-      if(score > 0)
-      {
-        document.getElementById(score).className = "plus"
-      }
-
-      score = score - 1;
-      this.setState({score: score})
-
-      document.getElementById(score).className = "token"
-    }
-
-    moveTokenForward(event) {
-      score = this.state.score
-      var score = parseInt(this.state.score)
-      
-      if(score < 0)
-      {
-        document.getElementById(score).className = "minus"
-      }
-      if(score === 0)
-      {
-        document.getElementById(score).className = "even"
-      }
-      if(score > 0)
-      {
-        document.getElementById(score).className = "plus"
-      }
-
-      score = score + 1;
-      this.setState({score: score})
-
-      document.getElementById(score).className = "token"
-    }
-
-
-   
+    score = score + 1;
+    this.setState({ score: score });
+    tokenLocation = this.state.id + score;
+    document.getElementById(tokenLocation).className = "token";
+  }
 
   render() {
-    console.log('props', this.props)
+    console.log("props", this.props);
     return (
-        <table className="sliderTable" id={this.props.id}>
-        <tr><th>{this.props.team}</th></tr>
+      <table className="sliderTable" id={this.props.id}>
+        <tr>
+          <th>{this.props.team}</th>
+        </tr>
         <button onClick={this.moveTokenBack}>Minus</button>
-        <tr><td>Minus</td></tr>
-        <tr id="-8" className="minus"><td>-8</td></tr>
-        <tr id="-7" className="minus"><td>-7</td></tr>
-        <tr id="-6" className="minus"><td>-6</td></tr>
-        <tr id="-5" className="minus"><td>-5</td></tr>
-        <tr id="-4" className="minus"><td>-4</td></tr>
-        <tr id="-3" className="minus"><td>-3</td></tr>
-        <tr id="-2" className="minus"><td>-2</td></tr>
-        <tr id="-1" className="minus"><td>-1</td></tr>
-        <tr id="0" className="even"><td>0</td></tr>
-        <tr id="1" className="plus"><td>+1</td></tr>
-        <tr id="2" className="plus"><td>+2</td></tr>
-        <tr id="3" className="plus"><td>+3</td></tr>
-        <tr id="4" className="plus"><td>+4</td></tr>
-        <tr id="5" className="plus"><td>+5</td></tr>
-        <tr id="6" className="plus"><td>+6</td></tr>
-        <tr id="7" className="plus"><td>+7</td></tr>
-        <tr id="8" className="plus"><td>+8</td></tr>
-        <tr><td>Plus</td></tr>
+        <tr>
+          <td>Minus</td>
+        </tr>
+        <tr id={this.props.id + "-8"} className="minus">
+          <td>-8</td>
+        </tr>
+        <tr id={this.props.id + "-7"} className="minus">
+          <td>-7</td>
+        </tr>
+        <tr id={this.props.id + "-6"} className="minus">
+          <td>-6</td>
+        </tr>
+        <tr id={this.props.id + "-5"} className="minus">
+          <td>-5</td>
+        </tr>
+        <tr id={this.props.id + "-4"} className="minus">
+          <td>-4</td>
+        </tr>
+        <tr id={this.props.id + "-3"} className="minus">
+          <td>-3</td>
+        </tr>
+        <tr id={this.props.id + "-2"} className="minus">
+          <td>-2</td>
+        </tr>
+        <tr id={this.props.id + "-1"} className="minus">
+          <td>-1</td>
+        </tr>
+        <tr id={this.props.id + "0"} className="even">
+          <td>0</td>
+        </tr>
+        <tr id={this.props.id + "1"} className="plus">
+          <td>+1</td>
+        </tr>
+        <tr id={this.props.id + "2"} className="plus">
+          <td>+2</td>
+        </tr>
+        <tr id={this.props.id + "3"} className="plus">
+          <td>+3</td>
+        </tr>
+        <tr id={this.props.id + "4"} className="plus">
+          <td>+4</td>
+        </tr>
+        <tr id={this.props.id + "5"} className="plus">
+          <td>+5</td>
+        </tr>
+        <tr id={this.props.id + "6"} className="plus">
+          <td>+6</td>
+        </tr>
+        <tr id={this.props.id + "7"} className="plus">
+          <td>+7</td>
+        </tr>
+        <tr id={this.props.id + "8"} className="plus">
+          <td>+8</td>
+        </tr>
+        <tr>
+          <td>Plus</td>
+        </tr>
         <button onClick={this.moveTokenForward}>Plus</button>
-        <tr><td>{this.state.score}</td></tr>
-        </table>
-
-        
+        <tr>
+          <td>{this.state.score}</td>
+        </tr>
+      </table>
     );
   }
 }
