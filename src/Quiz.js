cb622e5d7bcb6bc1f2a974a9b41be6b2133c3f23
Chilejon <jonchiles@yahoo.com>
2018-10-22 20:03:53 +0100

    Added add team

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,19 +2,42 @@ import React, { Component } from "react";
 import Slider from "./components/Slider";
 
 class Quiz extends Component {
-  state = {
-    slider: [
-      { id: "A", team: "team A", score: "0" }
-      //{ id: "B", team: "Tigers", score: "0" },
-      //{ id: "C", team: "DreamT", score: "0" },
-      //{ id: "D", team: "No Clue", score: "0" },
-      //{ id: "E", team: "BAs", score: "0" }
-    ]
-  };
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      slider: [],
+      showForm: false
+    };
+    this.addTeam = this.addTeam.bind(this);
+  }
+
+  addTeam(e) {
+    var newArray = this.state.slider.slice();
+    newArray.push({
+      id: this.teamName.value,
+      team: this.teamName.value,
+      score: "0"
+    });
+    this.setState({ slider: newArray });
+    e.preventDefault();
+  }
 
   render() {
     return (
       <div>
+        <form onSubmit={this.addTeam}>
+          <div>
+            <p>Enter a team</p>
+            <label>Team name</label>
+            <input
+              id="teamName"
+              ref={teamName => (this.teamName = teamName)}
+              required
+            />
+            <button type="submit">add</button>
+          </div>
+        </form>
+
         {this.state.slider.map(slider => (
           <section className="slider">
             <Slider id={slider.id} team={slider.team} score={slider.score} />
